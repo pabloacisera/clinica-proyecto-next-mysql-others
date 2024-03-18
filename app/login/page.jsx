4e7290195ab9c2dba349815c825e1a9b8bb87380
TuNombreDeUsuario <pabloacisera2021@gmail.com>
2024-03-17 22:43:14 -0300

    firts commit

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage(){
+  return(
+    <div>
+      <form action="#" method="POST">
+        <input type="text" name="user "placeholder="Ingrese usuario"/>
+        <input type="password" name="password" placeholder="password"/>
+        <button type="submit">ingresar</button>
+      </form>
+    </div>
+  )
+}
